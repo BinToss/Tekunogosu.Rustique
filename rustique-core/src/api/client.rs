e4831575670cd1a18ed825a4112256cde352be37
Noah Sherwin <noahrsherwin@gmail.com>
2026-05-02 13:57:28 -0700

    refactor(core.api.client): print errors w/ different `error!` arm
    This fixes the unexpectedly re-formatted error messages issue introduced in 4292bbe

--- rustique-core/src/api/client.rs
+++ rustique-core/src/api/client.rs
@@ -225,7 +225,7 @@ impl ApiClient {
 
         pb.finish_with_message("Fetch Complete");
         for err in error_messages.iter() {
-            error!(err)
+            error!("{}", err);
         }
         Ok(results)
     }
